b64d3fa41a56b99f5fe621c499915cd621d9dbaa
Dew0277 <jaloliddina98@gmail.com>
2024-09-30 02:44:11 -0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button variant={"destructive"} className="w-32">
+        Submit
+      </Button>
+    </>
+  );
+}
+
+export default App;
